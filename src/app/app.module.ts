515a2161dd722f6d7ebe233a0953c1af691d15eb
Siena Giacoma <sgiacoma2023@chadwickschool.org>
2021-10-19 16:53:40 -0700

    inital commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { CartComponent } from './components/shopping-cart/cart/cart.component';
+import { FiltersComponent } from './components/shopping-cart/filters/filters.component';
+import { ProductListComponent } from './components/shopping-cart/product-list/product-list.component';
+import { MessengerComponent } from './services/messenger/messenger.component';
+import { ProductComponent } from './services/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    NavComponent,
+    CartComponent,
+    FiltersComponent,
+    ProductListComponent,
+    MessengerComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
