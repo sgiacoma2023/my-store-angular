c94197d1cf026d6ca990bfad4efa996c721599ca
Siena Giacoma <sgiacoma2023@chadwickschool.org>
2021-10-27 10:57:45 -0700

    feat: add cart service

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../models/cart-item';
+import { Product } from '../models/product';
+import { MessengerService } from './messenger.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: Array<CartItem> = [
+    //{ id: 1, productId: 1, productName: 'test1', qty: 4, price: 20},
+    //{ id: 2, productId: 3, productName: 'test3', qty: 5, price: 25},
+    //{ id: 3, productId: 2, productName: 'test2', qty: 3, price: 20},
+    //{ id: 4, productId: 4, productName: 'test4', qty: 2, price: 35}
+  ];
+
+  cartTotal = 0;
+  constructor(private  msg:  MessengerService) { }
+
+
+  addProductToCart(product: Product) {
+    
+    let productExists  = false
+
+    for(let i in this.cartItems){
+      if(this.cartItems[i].productId === product.id) {
+        this.cartItems[i].qty++
+        productExists = true
+        break;
+      }
+    }
+    if(!productExists){
+      this.cartItems.push({
+        id: product.id,
+        productId: product.id,
+        productName: product.name, 
+        qty: 1,
+        price: product.price
+      })
+    }
+
+    this.cartTotal = 0
+    this.cartItems.forEach(item =>  {
+      this.cartTotal += (item.qty * item.price)
+    })
+    console.log(this.cartTotal);
+  }
+}
