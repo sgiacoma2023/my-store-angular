51770cf3275e9096935c8829fb16cdf558e1513f
Siena Giacoma <sgiacoma2023@chadwickschool.org>
2021-10-28 18:53:51 -0700

    feat: Add products

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -5,7 +5,7 @@ export class Product {
     price: number;
     imageURL: string;
 
-    constructor(id: number , name = ' ', description = ' ', price = 0, imageURL = 'https://scx2.b-cdn.net/gfx/news/2018/1-rubikscube.jpg'){
+    constructor(id: number , name = ' ', description = ' ', price = 0, imageURL = ''){
         this.id = id;
         this.name = name;
         this.description = description;
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -8,13 +8,12 @@ import { Product } from '../models/product'
 export class ProductService {
  
     products: Product[] = [
-      new Product(1, 'product 1', 'this  product is cool!', 10),
-      new Product(2, 'product 2', 'this  product is cool!', 15),
-      new Product(3, 'product 3', 'this  product is cool!', 20),
-      new Product(4, 'product 4', 'this  product is cool!', 25),
-      new Product(5, 'product 5', 'this  product is cool!', 30),
-      new Product(6, 'product 6', 'this  product is cool!', 35),
-      new Product(7, 'product 7', 'this  product is cool!', 40)
+      new Product(1, 'Blue Candle', 'Blue Candle', 5, 'https://images.thdstatic.com/productImages/5b1c99fe-7e70-463e-8737-248bb0365b13/svn/blue-zest-candle-candles-cpz-077-12-64_1000.jpg'),
+      new Product(2, 'Body Scrub', 'Body Scrub', 10, 'https://m.media-amazon.com/images/I/91noaNLihmL._SL1500_.jpg'),
+      new Product(3, 'Nail Polish', 'Nail Polish', 7, 'https://target.scene7.com/is/image/Target/GUEST_e6b4f362-2612-45cb-a56e-7916c3635b5b?wid=488&hei=488&fmt=pjpeg'),
+      new Product(4, 'Charm Bracelet', 'Charm Bracelet', 35, 'https://media-us.cdn.prod.wmgecom.com/media/catalog/product/cache/564/image/800x/9df78eab33525d08d6e5fb8d27136e95/0/8/082619_mm_k12_merch_bracelet.png'),
+      new Product(5, 'Crystal', 'Crystal', 9, 'https://www.crystalvaults.com/wp-content/uploads/2021/08/213654.jpg'),
+      new Product(6, 'Notebook', 'Notebook', 20, 'https://poppin.imgix.net/products/2020/poppin_coast_lagoon_work_happy_1-subjec_notebook_PDP_01.jpg'),
     ]
   constructor() { }
 
